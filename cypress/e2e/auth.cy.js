f1977eba072fdc7d4a828837b0eb1ffc61ffc1e6
Vladimir Palenov <palenov@gmail.com>
2023-01-07 23:13:33 -0800

    added test to log in with correct login and password to auth.cy.js file

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,5 +1,15 @@
 describe('empty spec', () => {
   it('passes', () => {
+    // Navigating to the login path
     cy.visit('/user/login')
+
+    // Typing in ligin, password and clicking 'Log in' button
+    cy.get('#normal_login_email').type('palenov@gmail.com')
+    cy.get('#normal_login_password').type('Sergey9659')
+    cy.get('[type="submit"]').click()
+
+    // Checking that correct path was opened and the page is rendered (has visible unique element)
+    cy.location('pathname').should('include', 'profile')
+    cy.get('.ant-avatar-square').should('be.visible')
   })
 })
