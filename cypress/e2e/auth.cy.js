de566ce71f104e9ccff23faab8dec1ed6bbd94c7
Vladimir Palenov <palenov@gmail.com>
2023-01-08 18:30:11 -0800

    added test to log in with correct login and password to auth.cy.js file

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,15 +1,41 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    // Navigating to the login path
+describe('Authentification', () => {
+  it('Log in with valid credentials', () => {
+    // Navigating to the login page
     cy.visit('/user/login')
 
-    // Typing in ligin, password and clicking 'Log in' button
+    // Typing in valid login and password, clicking 'Log in' button
     cy.get('#normal_login_email').type('palenov@gmail.com')
     cy.get('#normal_login_password').type('Sergey9659')
     cy.get('[type="submit"]').click()
 
-    // Checking that correct path was opened and the page is rendered (has visible unique element)
+    // Verifying that correct path was opened and the page is rendered (has visible unique element)
     cy.location('pathname').should('include', 'profile')
     cy.get('.ant-avatar-square').should('be.visible')
+  })
+  it('Log in with valid email and invalid password', () => {
+    // Navigating to the login path
+    cy.visit('/user/login')
+
+    // Typing in valid login and invalid password, clicking 'Log in' button
+    cy.get('#normal_login_email').type('palenov@gmail.com')
+    cy.get('#normal_login_password').type('Test123')
+    cy.get('[type="submit"]').click()
+
+    // Verifying that alert message 'Auth failed' pops up on the login page
+    cy.location('pathname').should('include', 'login')
+    cy.get('.ant-notification-notice-message').should('have.text', 'Auth failed').should('be.visible')
+  })
+  it('Log in with invalid email and password', () => {
+    // Navigating to the login page
+    cy.visit('/user/login')
+
+    // Typing in invalid login and password, clicking 'Log in' button
+    cy.get('#normal_login_email').type('pabcdef@ghijkl.com')
+    cy.get('#normal_login_password').type('W#4912Afm@')
+    cy.get('[type="submit"]').click()
+
+    // Verifying that alert message 'Auth failed' pops up on the login page
+    cy.location('pathname').should('include', 'login')
+    cy.get('.ant-notification-notice-message').should('have.text', 'Auth failed').should('be.visible')
   })
 })
